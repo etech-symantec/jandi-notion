abcfd1b8a7733896c6e198c2fac00c0b468221b5
etech-symantec <shin86@etechsystem.co.kr>
2026-09-18 23:41:52 +0900

    Update notion.js

--- api/notion.js
+++ api/notion.js
@@ -9,6 +9,9 @@ let bodyIndexCache = {
 };
 
 export default async function handler(req, res) {
+  console.log("=== JANDI REQUEST START ===");
+  console.log("method:", req.method);
+  console.log("body:", JSON.stringify(req.body));
   if (req.method === "GET") {
     return res.status(200).json({
       ok: true,
