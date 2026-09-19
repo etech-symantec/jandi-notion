a70fabc401411d52a90a08b268b16bd8ba3743c0
etech-symantec <shin86@etechsystem.co.kr>
2026-09-20 00:03:20 +0900

    Delete api/broadcom directory

--- api/broadcom/reindex.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-  return res.status(200).json({
-    ok: true,
-    service: "Broadcom KB Batch Reindex v4.1",
-    start: "/api/broadcom/start?token=REINDEX_TOKEN",
-    next: "/api/broadcom/next?token=REINDEX_TOKEN",
-    status: "/api/broadcom/status?token=REINDEX_TOKEN"
-  });
-}
